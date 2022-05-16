b16c15fab8c729bb1a9c485b596b2299aacdc697
Marc Zwiers <mczwiers@gmail.com>
2022-05-16 13:40:11 +0200

    Eerste build voor de nieuwe table manier

--- src/Components/SongOverview.js
+++ src/Components/SongOverview.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SongForm from "./SongForm";
+import SongList from "./SongList";
 
 
 class SongOverview extends React.Component {
@@ -17,23 +18,28 @@ class SongOverview extends React.Component {
         song.id = this.state.songs.length + 1
         this.setState({
           songs: [
-            ...this.state.songs, { song },
+            ...this.state.songs, song,
           ],
         });
+        console.log(this.state.songs)
       };
-      
+
     render() {
       return (
         <div>
                 <SongForm addSong={this.addSong}/>
-                                          <table style={{width: "100%"}}>
-                          <tr className="song-header">  
-                                  <th className="song-row__item">Song</th>
-                                  <th className="song-row__item">Artist</th>
-                                  <th className="song-row__item">Genre</th>
-                                  <th className="song-row__item">Rating</th>
-                                </tr>
-                                  </table>
+                  <table style={{width: "100%"}}>   
+                    <tbody>    
+                      <tr className="song-header">  
+                        <th className="song-row__item">Song</th>
+                        <th className="song-row__item">Artist</th>
+                        <th className="song-row__item">Genre</th>
+                        <th className="song-row__item">Rating</th>
+                      </tr>
+                    </tbody>
+                  </table>
+
+               <SongList songs={this.state.songs}/>
         </div>
       );
     }
